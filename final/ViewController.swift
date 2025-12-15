6221b5ea5661a3620a933fa3f058c15480a2f9df
anuar.oralov <anuar.oralov@kcell.kz>
2025-12-15 17:05:13 +0500

    Initial Commit

---
+++ final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  final
+//
+//  Created by Anuar Oralov on 15.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ finalTests/finalTests.swift
@@ -0,0 +1,17 @@
+//
+//  finalTests.swift
+//  finalTests
+//
+//  Created by Anuar Oralov on 15.12.2025.
+//
+
+import Testing
+@testable import final
+
+struct finalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
